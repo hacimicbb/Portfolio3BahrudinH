5691d7a77bba3cb7b6aabd095b5ed1543726d152
Bahrudin Hacimic <bhacimic10@yahoo.com>
2015-01-07 09:15:49 -0800

    1st push

---
+++ public_html/scriptjs.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$("document").ready(function(){
+    $(".epic").css("background-color", "yellow");
+    $('p:last').css({"background-color": "yellow", "color": "white"});
+});
