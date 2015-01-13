7945042e22fe92b92767aa9e4d0c813b9625a379
Bahrudin Hacimic <bhacimic10@yahoo.com>
2015-01-13 09:18:28 -0800

    Progressing Push

--- public_html/scriptjs.js
+++ public_html/scriptjs.js
@@ -4,23 +4,37 @@
  * and open the template in the editor.
  */
 
-$("document").ready(function(){
+$("document").ready(function() {
     $('#oneButton').bind('click', keyisClicked);
-    $('#logo').bind('click', mouseOverMe);
+    $('#logo').bind('mouseover', mouseOverMe);
+    $('p:last').css("background-color", "purple").css("color", "white");
 });
 
-    function keyisClicked(){
-        alert("There was a button clicked");
-    }
-    
-    function mouseOverMe()
-    {
-        alert("You put my cursor on my logo");
-    }
-    
-    function mouseOutMe()
-    {
-        ("#second").html("You put my cursor on my logo");
-    }
-    
+$("document").ready(function() {
+
+$('deleteSpan').bind('click', deleteSpan)
+ 
+     $('randPara').before('<span>Before Paragraph</span');
+     $('<span> Insert Before Paragraph</span>').insertBefore('randPara');
+     $('<span> Prepend to Paragraph</span>').prependTo('randPara');
+     $('<span> Append to Paragraph</span>')/appendTo('randPara');
+     $('#randPara').append('<span> Append Paragraph</span');
+     $('<span> Insert After Paragraph</span>').insertAfter('#randPara');
+});
+
+function keyisClicked() {
+    alert("There was a button clicked");
+}
+
+function mouseOverMe()
+{
+    console.log("mouseover");
+    $("#second").html("You put my cursor on my logo");
+}
+
+function mouseOutMe()
+{
+    $("#second").html("You took my cursor off my logo");
+}
+
     
